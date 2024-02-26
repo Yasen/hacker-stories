877d67429a3fcb37de4bc1bf8ba341b9b0f0b9a9
Yasen Petrov <yasenpetrov@gmail.com>
2024-02-26 09:42:00 +0200

    Callback Handlers In JSX

--- src/App.jsx
+++ src/App.jsx
@@ -21,21 +21,30 @@ const App = () => {
     },
   ];
 
+  // A
+  const handleSearch = (event) => {
+    // D
+    console.log(event.target.value);
+  }
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search/>
+      {/* B */}
+      <Search onSearch={handleSearch} />
       <hr/>
       <List list={stories} />
     </div>
   );
 }
 
-const Search = () => {
+const Search = (props) => {
   const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
+    // C
+    props.onSearch(event);
   };
 
   return (
