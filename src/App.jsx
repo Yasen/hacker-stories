4208ab2d9fabf6b18b81bd742994237b3396b6d8
Yasen Petrov <yasenpetrov@gmail.com>
2024-03-01 18:51:35 +0200

    React Fragments 2

--- src/App.jsx
+++ src/App.jsx
@@ -55,18 +55,17 @@ const App = () => {
   );
 };
 
-const Search = ({search, onSearch}) => [
-    <label key="1" htmlFor="search">
-        Search: {' '}
-    </label>,
+const Search = ({search, onSearch}) => (
+  <React.Fragment>
+    <label htmlFor="search">Search: </label>
     <input
-      key="2"
       id="search"
       type="text"
       value={search}
       onChange={onSearch}
-    />,
-];
+    />
+  </React.Fragment>
+);
 
 const List = ({list}) => (
   <ul>
