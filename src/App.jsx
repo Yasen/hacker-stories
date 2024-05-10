57a3bf1191b7a0927273aa15c36fe49d448f5aa9
Yasen Petrov <yasenpetrov@gmail.com>
2024-05-10 19:15:25 +0300

    Update App.jsx
    Data re-fetching in React

--- src/App.jsx
+++ src/App.jsx
@@ -59,9 +59,15 @@ const App = () => {
   );
 
   React.useEffect(() => {
+    // if `searchTerm` is not presesnt
+    // e.g. null, empty string, undefined
+    // do nothing
+    // more generalized condition than searchTerm === ''
+    if (!searchTerm) return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -72,7 +78,7 @@ const App = () => {
       .catch(() =>
         dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
       );
-  }, []);
+  }, [searchTerm]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -84,10 +90,6 @@ const App = () => {
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-
-  const searchedStories = stories.data.filter((story) =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   return (
     <div>
@@ -110,7 +112,7 @@ const App = () => {
         <p>Loading ...</p>
       ) : (
         <List
-          list={searchedStories}
+          list={stories.data}
           onRemoveItem={handleRemoveStory}
         />
       )}
